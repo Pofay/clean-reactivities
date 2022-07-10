7ae88da3ab6ea3a7da4318ad3f32e8a82bff7644
Pofay <gianCarlo.gilos@gmail.com>
2022-07-10 14:29:12 +0800

    Added fp-ts library and started implementing integration with API

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
+
+const tap =
+  (f: (a: any) => void) =>
+  (v: any): any => {
+    f(v)
+    return v
+  }
 
 function App() {
+  const [activities, setActivities] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5272/api/activities')
+      .then((res) => res.data)
+      .then(tap((a) => console.log(a)))
+      .then(setActivities)
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <ul>
+          {activities.map((a: any) => (
+            <li key={a.id}>{a.title}</li>
+          ))}
+        </ul>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
