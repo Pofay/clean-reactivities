adb3f8346cd938ca6258e5dc7d76eb268feb8779
Gian Carlo Gilos <gian@isr.com.ph>
2022-07-12 17:06:12 +0800

    Used MediatR handle for Listing all Activities

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -6,28 +6,30 @@ using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using MediatR;
+using Application.Activities;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _dbContext;
+        private readonly IMediator _mediator;
 
-        public ActivitiesController(DataContext dbContext)
+        public ActivitiesController(IMediator mediator)
         {
-            _dbContext = dbContext;
+            _mediator = mediator;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _dbContext.Activities.ToListAsync();
+            return await _mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _dbContext.Activities.FindAsync(id);
+            return Ok();
         }
     }
 }
