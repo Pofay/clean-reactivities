341a3cd60a85231c4495c99b7aaa953d8864ef6c
Gian Carlo Gilos <gian@isr.com.ph>
2022-07-12 17:24:24 +0800

    Fixed Assembly to use Application instead of Startup when registering MediatR

--- API/Startup.cs
+++ API/Startup.cs
@@ -47,7 +47,7 @@ namespace API
                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
                 });
             });
-            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddMediatR(typeof(ListActivities.Handler).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
