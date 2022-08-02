c5f65e5f4504ded1db40b9c313b8ed669725e08f
Gian Carlo Gilos <gian@isr.com.ph>
2022-08-02 14:06:23 +0800

    Initial setup for react-router

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App/Layout/App'
-import './App/Layout/styles.css'
-import { store, StoreContext } from './App/stores/store'
-import reportWebVitals from './reportWebVitals'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App/Layout/App';
+import './App/Layout/styles.css';
+import { store, StoreContext } from './App/stores/store';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <StoreContext.Provider value={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </StoreContext.Provider>
   </React.StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
