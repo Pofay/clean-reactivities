a983429b2e58a372556a030d6cb26003fa936122
Pofay <gianCarlo.gilos@gmail.com>
2022-11-22 22:59:30 +0800

    Added NotFound component as catch-all when going to an unknown route

--- client-app/src/App/Layout/App.tsx
+++ client-app/src/App/Layout/App.tsx
@@ -5,14 +5,16 @@ import HomePage from '../../Features/home/HomePage'
 import { Route, Routes } from 'react-router-dom'
 import TestErrors from '../../Features/Errors/TestError'
 import { ToastContainer } from 'react-toastify'
+import NotFound from '../../Features/Errors/NotFound'
 
 function App() {
   return (
     <>
       <ToastContainer position='bottom-right' hideProgressBar />
       <Routes>
+        <Route path='*' element={<NotFound />} />
         <Route path='/' element={<HomePage />} />
-        <Route path='/errors' element={<TestErrors />} />
+        <Route path='errors' element={<TestErrors />} />
         <Route path='activities/*' element={<ActivityDashboard />} />
       </Routes>
     </>
