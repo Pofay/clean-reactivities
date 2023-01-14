dab173d90ee0311ac93744e668c65fa375726b06
Pofay <gianCarlo.gilos@gmail.com>
2023-01-14 01:32:09 +0000

    Provided registration route

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,5 +6,6 @@ namespace API.DTOs
         public string Password { get; set; }
         public string DisplayName { get; set; }
         public string Username { get; set; }
+        public string Bio { get; set;}
     }
 }
