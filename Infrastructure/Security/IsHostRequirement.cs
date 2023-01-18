05d56a1d312eab0ddc16e7985c4d921bb71eab85
Pofay <gianCarlo.gilos@gmail.com>
2023-01-18 23:22:17 +0000

    Updated authorization middleware to prevent EF Core from Tracking Attendees to prevent the property from being empty on user edit

--- Infrastructure/Security/IsHostRequirement.cs
+++ Infrastructure/Security/IsHostRequirement.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Infrastructure.Security
@@ -28,7 +29,7 @@ namespace Infrastructure.Security
             if (userId == null) return Task.CompletedTask;
 
             var activityId = Guid.Parse(_httpContextAccessor.HttpContext?.Request.RouteValues.SingleOrDefault(x => x.Key == "id").Value?.ToString());
-            var attendee = _context.ActivityAttendees.FindAsync(userId, activityId).Result;
+            var attendee = _context.ActivityAttendees.AsNoTracking().SingleOrDefaultAsync(x => x.AppUserId == userId && x.ActivityId == activityId).Result;
 
             if (attendee == null) return Task.CompletedTask;
 
