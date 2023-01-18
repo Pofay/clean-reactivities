02716b7f3a1d2c4fb89ea93760a51526897788a4
Pofay <gianCarlo.gilos@gmail.com>
2023-01-18 23:12:05 +0000

    Added authorization middleware to check if user is host for use in editing and deleting activity endpoints

---
+++ Infrastructure/Security/IsHostRequirement.cs
@@ -0,0 +1,40 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Persistence;
+
+namespace Infrastructure.Security
+{
+    public class IsHostRequirement : IAuthorizationRequirement
+    {
+    }
+
+    public class IsHostRequirementHandler : AuthorizationHandler<IsHostRequirement>
+    {
+        private readonly DataContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public IsHostRequirementHandler(DataContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsHostRequirement requirement)
+        {
+            var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (userId == null) return Task.CompletedTask;
+
+            var activityId = Guid.Parse(_httpContextAccessor.HttpContext?.Request.RouteValues.SingleOrDefault(x => x.Key == "id").Value?.ToString());
+            var attendee = _context.ActivityAttendees.FindAsync(userId, activityId).Result;
+
+            if (attendee == null) return Task.CompletedTask;
+
+            if (attendee.IsHost) context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
