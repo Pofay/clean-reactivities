4e7f59b82d457e498c86c274e9d330cd958190e8
Pofay <gianCarlo.gilos@gmail.com>
2023-01-23 23:40:38 +0000

    Added conditional rendering for various buttons if user is either an attendee or host

--- client-app/src/Features/Activities/details/ActivityDetailedSidebar.tsx
+++ client-app/src/Features/Activities/details/ActivityDetailedSidebar.tsx
@@ -1,13 +1,15 @@
-import { Segment, List, Label, Item, Image } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
-import { UserProfile } from '../../../App/models/interfaces/profile';
+import { Link } from 'react-router-dom';
+import { Image, Item, Label, List, Segment } from 'semantic-ui-react';
+import { Activity } from '../../../App/models/interfaces/activity';
 
 interface Props {
-  attendees: UserProfile[];
+  activity: Activity;
 }
 
 export default observer(function ActivityDetailedSidebar(props: Props) {
+  const { attendees, host } = props.activity;
+  if (!attendees) return null;
   return (
     <>
       <Segment
@@ -18,20 +20,21 @@ export default observer(function ActivityDetailedSidebar(props: Props) {
         inverted
         color='teal'
       >
-        {props.attendees.length}{' '}
-        {props.attendees.length === 1 ? 'Person' : 'People'} going
+        {attendees.length} {attendees.length === 1 ? 'Person' : 'People'} going
       </Segment>
       <Segment attached>
         <List relaxed divided>
-          {props.attendees.map((attendee) => (
+          {attendees.map((attendee) => (
             <Item style={{ position: 'relative' }} key={attendee.userName}>
-              <Label
-                style={{ position: 'absolute' }}
-                color='orange'
-                ribbon='right'
-              >
-                Host
-              </Label>
+              {attendee.userName === host?.userName && (
+                <Label
+                  style={{ position: 'absolute' }}
+                  color='orange'
+                  ribbon='right'
+                >
+                  Host
+                </Label>
+              )}
               <Image size='tiny' src={attendee.image || '/assets/user.png'} />
               <Item.Content verticalAlign='middle'>
                 <Item.Header as='h3'>
