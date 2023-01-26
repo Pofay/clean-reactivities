af3c657f24078c5da0f3c7fd8eeba7d0a85f00e2
Pofay <gianCarlo.gilos@gmail.com>
2023-01-26 22:43:37 +0000

    Updated index to use Absolute imports

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'react-calendar/dist/Calendar.css'
-import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
-import 'react-toastify/dist/ReactToastify.min.css'
-import App from './App/Layout/App'
-import './App/Layout/styles.css'
-import { store, StoreContext } from './App/stores/store'
-import reportWebVitals from './reportWebVitals'
-import { createBrowserHistory } from 'history'
-import 'react-datepicker/dist/react-datepicker.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'react-calendar/dist/Calendar.css';
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.min.css';
+import App from 'App/Layout/App';
+import 'App/Layout/styles.css';
+import { store, StoreContext } from 'App/stores/store';
+import reportWebVitals from './reportWebVitals';
+import { createBrowserHistory } from 'history';
+import 'react-datepicker/dist/react-datepicker.css';
 
-export const history = createBrowserHistory()
+export const history = createBrowserHistory();
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <StoreContext.Provider value={store}>
@@ -21,9 +23,9 @@ root.render(
       </HistoryRouter>
     </StoreContext.Provider>
   </React.StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
