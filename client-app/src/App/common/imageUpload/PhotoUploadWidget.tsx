6bd697e69d6c306292a6f9b69a246cf45000fe68
Pofay <gianCarlo.gilos@gmail.com>
2023-02-08 00:41:15 +0000

    feat(incomplete): created a skeleton for dropzone logic in PhotoUploadWidget component

--- client-app/src/App/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/App/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,3 +1,4 @@
+import PhotoWidgetDropzone from 'App/common/imageUpload/PhotoWidgetDropzone';
 import { Grid, Header } from 'semantic-ui-react';
 
 function PhotoUploadWidget() {
@@ -5,6 +6,7 @@ function PhotoUploadWidget() {
     <Grid>
       <Grid.Column width={4}>
         <Header sub color='teal' content='Step 1 - Add Photo' />
+        <PhotoWidgetDropzone />
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
