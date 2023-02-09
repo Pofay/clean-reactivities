5b14b472d3cd8192aa320383f52403a30799f2a9
Pofay <gianCarlo.gilos@gmail.com>
2023-02-09 01:37:51 +0000

    feat(incomplete): Added API endpoints to set main photo or delete selected photo

--- client-app/src/App/api/agent.ts
+++ client-app/src/App/api/agent.ts
@@ -95,6 +95,8 @@ const Profiles = {
       headers: { 'Content-Type': 'multipart/form-data' },
     });
   },
+  setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+  deletePhoto: (id: string) => requests.delete(`/photos/${id}`),
 };
 
 const Account = {
