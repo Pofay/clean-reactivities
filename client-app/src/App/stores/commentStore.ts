c30b2d88364bca931f4aefe69dd02e997c725135
Pofay <gianCarlo.gilos@gmail.com>
2023-02-16 01:30:04 +0000

    feat(in-progress): created comment and its associated store with signalR connection hooks

---
+++ client-app/src/App/stores/commentStore.ts
@@ -0,0 +1,64 @@
+import {
+  HubConnection,
+  HubConnectionBuilder,
+  LogLevel,
+} from '@microsoft/signalr';
+import { ChatComment } from 'App/models/interfaces/comment';
+import { store } from 'App/stores/store';
+import { makeAutoObservable, runInAction } from 'mobx';
+
+class CommentStore {
+  comments: ChatComment[] = [];
+  hubConnection: HubConnection | null = null;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  createAndStartHubConnection = (activityId: string) => {
+    if (store.activityStore.selectedActivity) {
+      this.hubConnection = this.createHubConnection(activityId);
+
+      this.hubConnection
+        .start()
+        .catch((error) =>
+          console.error('Error starting SignalR Connection to Server', error)
+        );
+
+      this.configureHubConnection(this.hubConnection);
+    }
+  };
+
+  stopHubConnection = () => {
+    this.hubConnection
+      ?.stop()
+      .catch((error) => console.error('Error stopping connection', error));
+  };
+
+  clearComments = () => {
+    this.comments = [];
+    this.stopHubConnection();
+  };
+
+  private configureHubConnection(hubConnection: HubConnection) {
+    hubConnection.on('LoadComments', (comments: ChatComment[]) => {
+      runInAction(() => (this.comments = comments));
+    });
+
+    hubConnection.on('ReceiveComment', (comment: ChatComment) => {
+      runInAction(() => this.comments.push(comment));
+    });
+  }
+
+  private createHubConnection(activityId: string) {
+    return new HubConnectionBuilder()
+      .withUrl(`http://localhost:5272/chat?activityId=${activityId}`, {
+        accessTokenFactory: () => store.userStore.user?.token!,
+      })
+      .withAutomaticReconnect()
+      .configureLogging(LogLevel.Information)
+      .build();
+  }
+}
+
+export default CommentStore;
--- client-app/src/App/stores/store.ts
+++ client-app/src/App/stores/store.ts
@@ -1,4 +1,5 @@
 import ActivityStore from 'App/stores/activityStore';
+import CommentStore from 'App/stores/commentStore';
 import CommonStore from 'App/stores/commonStore';
 import ModalStore from 'App/stores/modalStore';
 import UserProfileStore from 'App/stores/userProfileStore';
@@ -9,6 +10,7 @@ interface Store {
   activityStore: ActivityStore;
   commonStore: CommonStore;
   userStore: UserStore;
+  commentStore: CommentStore;
   modalStore: ModalStore;
   userProfileStore: UserProfileStore;
 }
@@ -17,6 +19,7 @@ export const store: Store = {
   activityStore: new ActivityStore(),
   commonStore: new CommonStore(),
   userStore: new UserStore(),
+  commentStore: new CommentStore(),
   modalStore: new ModalStore(),
   userProfileStore: new UserProfileStore(),
 };
