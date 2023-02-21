460322d3bfe292599ed054ec00305ee2d11d86a0
Pofay <gianCarlo.gilos@gmail.com>
2023-02-21 06:21:55 +0000

    chore: removed class keyword for Dto records in API:

--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -1,7 +1,7 @@
 namespace API.DTOs
 {
 
-    public record class LoginDto
+    public record LoginDto
     {
         public string Email { get; init; }
         public string Password { get; init; }
--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -1,6 +1,6 @@
 namespace API.DTOs
 {
-    public record class UserDto
+    public record UserDto
     {
         public string DisplayName { get; init; }
         public string Token { get; init; }
