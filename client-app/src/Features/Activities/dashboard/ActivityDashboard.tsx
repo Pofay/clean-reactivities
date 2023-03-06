e4c5cca347af0cc4dd5b8686012151b86a8e16ce
Pofay <gianCarlo.gilos@gmail.com>
2023-03-06 10:19:10 +0000

    feat: Added vertical paging to when displaying list of activities

--- client-app/src/Features/Activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/Features/Activities/dashboard/ActivityDashboard.tsx
@@ -4,24 +4,33 @@ import ActivityList from 'Features/Activities/dashboard/ActivityList';
 import ActivityDetails from 'Features/Activities/details/ActivityDetails';
 import ActivityForm from 'Features/Activities/form/ActivityForm';
 import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 function ActivityDashboard() {
   const { activityStore } = useStore();
   const { activityRegistry, loadActivities } = activityStore;
+  const [loadingNext, setLoadingNext] = useState(false);
 
   useEffect(() => {
     if (activityRegistry.size <= 1) loadActivities();
   }, [activityRegistry.size, loadActivities]);
 
-  if (activityStore.loadingInitial)
+  if (activityStore.loadingInitial && !loadingNext)
     return <LoadingComponent content='Loading activities...' />;
 
   return (
     <>
       <Routes>
-        <Route index element={<ActivityList />} />
+        <Route
+          index
+          element={
+            <ActivityList
+              loadingNext={loadingNext}
+              setLoadingNext={setLoadingNext}
+            />
+          }
+        />
         <Route path=':id' element={<ActivityDetails />} />
         <Route path='createActivity' element={<ActivityForm />} />
         <Route path='manage/:id' element={<ActivityForm />} />
