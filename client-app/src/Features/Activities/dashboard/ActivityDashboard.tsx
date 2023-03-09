457eb10159ca81fceb011b547bc42535ffcc749b
Pofay <gianCarlo.gilos@gmail.com>
2023-03-09 03:12:48 +0000

    feat: Fully updated project to use latest react-router-domv6 changes

--- client-app/src/Features/Activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/Features/Activities/dashboard/ActivityDashboard.tsx
@@ -1,16 +1,11 @@
-import LoadingComponent from 'App/Layout/LoadingComponent';
 import { useStore } from 'App/stores/store';
-import ActivityList from 'Features/Activities/dashboard/ActivityList';
-import ActivityDetails from 'Features/Activities/details/ActivityDetails';
-import ActivityForm from 'Features/Activities/form/ActivityForm';
 import { observer } from 'mobx-react-lite';
-import { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
 
 function ActivityDashboard() {
   const { activityStore } = useStore();
   const { activityRegistry, loadActivities } = activityStore;
-  const [loadingNext, setLoadingNext] = useState(false);
 
   useEffect(() => {
     if (activityRegistry.size <= 1) loadActivities();
@@ -18,20 +13,7 @@ function ActivityDashboard() {
 
   return (
     <>
-      <Routes>
-        <Route
-          index
-          element={
-            <ActivityList
-              loadingNext={loadingNext}
-              setLoadingNext={setLoadingNext}
-            />
-          }
-        />
-        <Route path=':id' element={<ActivityDetails />} />
-        <Route path='createActivity' element={<ActivityForm />} />
-        <Route path='manage/:id' element={<ActivityForm />} />
-      </Routes>
+      <Outlet />
     </>
   );
 }
